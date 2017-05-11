992fb82880bd8b75076a8aef102b9bb5e2035990
Will Laco <wlaco27@gmail.com>
2017-05-10 22:36:54 -0400

    created the coreData adding and viewing

--- GameCollection/AddGameViewController.swift
+++ GameCollection/AddGameViewController.swift
@@ -27,10 +27,31 @@ class AddGameViewController: UIViewController, UIImagePickerControllerDelegate, 
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
     @IBAction func cameraPressed(_ sender: Any) {
     }
     
     @IBAction func AddPressed(_ sender: Any) {
+    
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let game = Game(context: context)
+        
+        game.title = AddGameTitleTextField.text
+        game.image = UIImagePNGRepresentation(imageView.image!)! as NSData
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    
     }
     
 }
--- GameCollection/ViewController.swift
+++ GameCollection/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var gameTableView: UITableView!
 
+    var games : [Game] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        gameTableView.dataSource = self
+        gameTableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            games = try context.fetch(Game.fetchRequest())
+            gameTableView.reloadData()
+        } catch {
+    
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let game = games[indexPath.row]
+        cell.textLabel?.text = game.title
+        cell.imageView?.image = UIImage(data: game.image! as Data)
+        return cell
+    }
 }
 
